67565c89ddf19fa1536f5d261d740929a796e6f9
wdong <wdong@iastate.edu>
2017-09-21 17:10:47 -0500

    Initial project commit

---
+++ src/main/java/com/webskeleton/WebskeletonApplication.java
@@ -0,0 +1,12 @@
+package com.webskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebskeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebskeletonApplication.class, args);
+	}
+}
